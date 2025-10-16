90d2e9f46e50339c805d85a817c5d73440352fcd
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-16 20:00:57 +0800

    Transferred: Dashboard.cs to Dashboard Folder. Transferred: Internals.cs to Global Classes folder.

--- obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4229774dc56bd4f0beccd25ef09a52f8d3acaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43af4796846ddf512b0da6636c9c1728ded79de4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
