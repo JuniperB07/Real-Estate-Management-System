efa58c7f009f01673e6a8da34bd4ae707c2ad0fa
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-19 16:03:49 +0800

    Added: New Form - EditPlan.cs Added: EditPlan.Methods.cs Updated: Project's .csproj file: Set EditPlan.Methods.cs dependency to EditPlan.cs

--- obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64e8eec41b468747548c457a8cc931159c0ddc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492386ea68db03ecfef6a0add601d04375432323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
