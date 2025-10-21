0b02df00bcf54002bb8c943d516838a7e60193e3
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-21 17:49:57 +0800

    minor change

--- obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Real Estate Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08abf0c83934c39dd5f3f8b31f1727bb6867a154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de07cd472e34744495aca0f2c035ef03253dc49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Real Estate Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
