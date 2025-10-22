bf2bd971623fe202950613403c17643f3193d33d
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-22 09:58:40 +0800

    Added method to provide capability to change value of Billing Configuration - Billing:InvoiceNumberPrefix.

--- Configs/Billing Config Reader/ChangeValues.cs
+++ Configs/Billing Config Reader/ChangeValues.cs
@@ -34,5 +34,19 @@ namespace Real_Estate_Management_System.Configs.Billing
                 }
             }
         }
+        internal static void Change_InvoiceNumberPrefix(string SetPrefix)
+        {
+            XElement? target;
+
+            target = Doc
+                .Descendants("add")
+                .FirstOrDefault(x => x.Attribute("key")?.Value == "Billing:InvoiceNumberPrefix");
+
+            if(target != null)
+            {
+                target?.SetAttributeValue("value", SetPrefix);
+                Doc.Save(configPath);
+            }
+        }
     }
 }
