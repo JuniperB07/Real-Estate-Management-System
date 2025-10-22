09365aca77a3d40254259d1bb59bd91ee3959e71
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-22 09:13:07 +0800

    Added GET Property: bool Billing.BHelper.IncludeInternet

--- Billing/BHelper.cs
+++ Billing/BHelper.cs
@@ -8,6 +8,7 @@ namespace Real_Estate_Management_System.Billing
 {
     internal static class BHelper
     {
+        internal static bool IncludeInternet { get { return Configs.Billing.BillingConfig.IncludeInternet; } }
         internal static InvoiceMetadata NewInvoice { get; set; }
     }
 }
