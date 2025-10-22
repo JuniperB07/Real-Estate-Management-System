7946a95e0677de3e9fc74007d1effd7ed4101903
Juniper Bolabon <34428689+JuniperB07@users.noreply.github.com>
2025-10-22 10:09:33 +0800

    Added GET property 'TenantName' in Billing.BHelper (BHelper.cs)

--- Billing/BHelper.cs
+++ Billing/BHelper.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JunX.NETStandard.MySQL;
+using JunX.NETStandard.SQLBuilder;
 
 namespace Real_Estate_Management_System.Billing
 {
     internal static class BHelper
     {
-        internal static bool IncludeInternet { get { return Configs.Billing.BillingConfig.IncludeInternet; } }
         internal static InvoiceMetadata NewInvoice { get; set; }
         internal static int TenantID { get; set; }
+
+        internal static bool IncludeInternet { get { return Configs.Billing.BillingConfig.IncludeInternet; } }
+        internal static string TenantName
+        {
+            get
+            {
+                new SelectCommand<tbtenants>()
+                    .Select(tbtenants.FullName)
+                    .From
+                    .StartWhere
+                        .Where(tbtenants.TenantID, SQLOperator.Equal, TenantID.ToString())
+                    .EndWhere
+                    .ExecuteReader(Internals.DBC);
+                return Internals.DBC.Values[0];
+            }
+        }
     }
 }
